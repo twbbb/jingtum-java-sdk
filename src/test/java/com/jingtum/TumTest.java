5948b7e33f2e76d9ba2f6f93a98b0e027d78b260
mdranger <lizhengpeng@jingtum.com>
2017-02-08 00:08:38 -0500

    Used the master branch to fix the compile issue. Added OperationClass, FinGate. Revised the TumTest.java, but still has issues.

--- src/test/java/com/jingtum/TumTest.java
+++ src/test/java/com/jingtum/TumTest.java
@@ -10,32 +10,36 @@ import com.jingtum.exception.FailedException;
 import com.jingtum.exception.InvalidParameterException;
 import com.jingtum.exception.InvalidRequestException;
 import com.jingtum.model.*;
-import com.jingtum.net.JingtumAPIAndWSServer;
-import com.jingtum.net.JingtumFingate;
+import com.jingtum.model.*;
+import com.jingtum.net.FinGate;
 
 //http://developer.jingtum.com/tongtong-start.html
 
 public class TumTest {
-	public static void main(String[] args) throws AuthenticationException, InvalidRequestException, APIConnectionException, ChannelException, APIException, FailedException, InvalidParameterException{
-		JingtumFingate.getTestInstance().setFinGate("js4UaG1pjyCEi9f867QHJbWwD3eo6C5xsa", "snqFcHzRe22JTM8j7iZVpQYzxEEbW"); //FinGate地址密码
-		JingtumFingate.getTestInstance().setCustom("00000003");  //设置用户编号
-		JingtumFingate.getTestInstance().setCustomSecret("feef59f67303bf7d0b8dcbb1cc99e802b937ff87"); //用户密码
-		String orderNumber = JingtumAPIAndWSServer.getTestInstance().getNextUUID(); //获得唯一订单号
+	public static void main(String[] args) throws AuthenticationException, InvalidRequestException,
+			APIConnectionException, ChannelException, APIException, FailedException, InvalidParameterException {
+		//FinGate fg = FinGate.getInstance();
+		FinGate.getInstance().setMode(1);
+		FinGate.getInstance().setAccount("js4UaG1pjyCEi9f867QHJbWwD3eo6C5xsa", "snqFcHzRe22JTM8j7iZVpQYzxEEbW"); //FinGate地址密码
+		FinGate.getInstance().setToken("00000003");  //设置用户编号
+		FinGate.getInstance().setKey("feef59f67303bf7d0b8dcbb1cc99e802b937ff87"); //用户密码
+
+		String order_id = "thisorderid01"; //获得唯一订单号
 
 		System.out.println("================issue custom tum================");
-		//Wallet wallet = JingtumFingate.getTestInstance().createWallet();
+		//Wallet wallet = FinGate.getInstance().createWallet();
 		//used an existing wallet instead
-		Wallet wallet = new Wallet("js4UaG1pjyCEi9f867QHJbWwD3eo6C5xsa","snqFcHzRe22JTM8j7iZVpQYzxEEbW");
+		Wallet wallet = new Wallet("snqFcHzRe22JTM8j7iZVpQYzxEEbW");
 		System.out.println(wallet.getAddress());
 		System.out.println(wallet.getSecret());
-		//JingtumFingate.getTestInstance().activateWallet(wallet.getAddress());
+		//FinGate.getInstance().activateWallet(wallet.getAddress());
 
-		boolean isSuccessful = JingtumFingate.getTestInstance().issueCustomTum(orderNumber, "8100000003000020160022201800220020000001", 11.27, wallet.getAddress());
+		boolean isSuccessful = FinGate.getInstance().issueCustomTum(order_id, "8100000003000020160022201800220020000001", 11.27, wallet.getAddress());
 		System.out.println(isSuccessful);
 
-		
+
 		System.out.println("================wallet balance================");
-		
+
 		try {
 			Thread.sleep(10000);  //等待发行通成功
 		} catch (InterruptedException e) {
@@ -43,17 +47,16 @@ public class TumTest {
 			e.printStackTrace();
 		}
 		BalanceCollection bc = wallet.getBalance();
-		
+
 		Balance bl;
 		Iterator<Balance> it = bc.getData().iterator();
-		while(it.hasNext())
-		{	
-		    bl = (Balance)it.next();
-		    System.out.println(bl.getCurrency());
-		    System.out.println(bl.getCounterparty());
-		    System.out.println(bl.getValue());
+		while (it.hasNext()) {
+			bl = (Balance) it.next();
+			System.out.println(bl.getCurrency());
+			System.out.println(bl.getCounterparty());
+			System.out.println(bl.getValue());
 		}
-		
+
 		System.out.println("================query custom tum issue================");
 		try {
 			Thread.sleep(5000);  //等待发行通成功
@@ -61,9 +64,9 @@ public class TumTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-                //Another test order number is
-		//IssueRecord ir = JingtumFingate.getTestInstance().queryIssue("prefix1479331641000001");
-                IssueRecord ir = JingtumFingate.getTestInstance().queryIssue(orderNumber);
+		//Another test order number is
+		//IssueRecord ir = FinGate.getInstance().queryIssue("prefix1479331641000001");
+		IssueRecord ir = FinGate.getInstance().queryIssue(order_id);
 		/*System.out.println(ir.getAccount());
 		System.out.println(ir.getAmount());
 		System.out.println(ir.getCurrency());
@@ -71,11 +74,11 @@ public class TumTest {
 		System.out.println(ir.getOrder());
 
 		System.out.println(ir.getTxHash());*/
-		System.out.println("res:"+ir.getStatus());
-		
+		System.out.println("res:" + ir.getStatus());
+
 		System.out.println("================query custom tum status================");
-		TumInfo ti = JingtumFingate.getTestInstance().queryCustomTum("8300000027000020160415201704150120000003");
-		System.out.println("res name: "+ti.getName());
+		TumInfo ti = FinGate.getInstance().queryCustomTum("8300000027000020160415201704150120000003");
+		System.out.println("res name: " + ti.getName());
 		/*System.out.println(ti.getCurrency());
 		System.out.println(ti.getName());
 		System.out.println(ti.getCirculation());
